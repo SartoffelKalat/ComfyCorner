daeea18007ffce5a8d52e67c65ec0a46e60f100f
DampMoss <yassin866@gmail.com>
2016-08-16 20:37:36 +0200

    test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from unit import *
 
 pygame.init()
-screen = pygame.display.set_mode((400, 300))
+screen = pygame.display.set_mode((600, 300))
 done = False
 
 hero = Unit(30, 30, 60, 60)
@@ -15,10 +15,10 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP]: hero.move_up(move_speed_hero)
-    if pressed[pygame.K_DOWN]: hero.move_down(move_speed_hero)
-    if pressed[pygame.K_LEFT]: hero.move_left(move_speed_hero)
-    if pressed[pygame.K_RIGHT]: hero.move_right(move_speed_hero)
+    if pressed[pygame.K_W]: hero.move_up(move_speed_hero)
+    if pressed[pygame.K_S]: hero.move_down(move_speed_hero)
+    if pressed[pygame.K_A]: hero.move_left(move_speed_hero)
+    if pressed[pygame.K_D]: hero.move_right(move_speed_hero)
     screen.fill((0, 0, 0))
     pygame.draw.rect(screen, (0, 128, 255), hero.get_rectangle())
 
